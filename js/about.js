4ea77afb7f828df425ccb4c714ffb8fb90b278a4
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2016-12-22 22:07:05 -0500

    Fixed about page and added effect for index

---
+++ js/about.js
@@ -0,0 +1,20 @@
+$(document).ready(
+    function() {
+        $("#interests").hover(function() {
+            $("#interestsinfo").toggle();
+        });
+});
+
+$(document).ready(
+    function() {
+        $("#about").hover(function() {
+            $("#aboutinfo").toggle();
+        });
+});
+
+$(document).ready(
+    function() {
+        $("#leadership").hover(function() {
+            $("#leadershipinfo").toggle();
+        });
+});
